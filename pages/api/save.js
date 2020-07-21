625df4f399204411842ac457ea9f7e4cd50b219f
Paulo Henrique Barbosa Santos <paulo_hbs17@hotmail.com>
2020-07-21 16:54:46 -0300

    adding stabilized base64

--- pages/api/save.js
+++ pages/api/save.js
@@ -15,7 +15,7 @@ export default async (req, res) => {
 
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key: process.env.SHEET_PRIVATE_KEY
+      private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
     })
     await doc.loadInfo();
 
